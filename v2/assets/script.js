2411b821b46ee1830640002fd00c64b4de62c3ca
Jungwoo Lee <jungwoo33@gmail.com>
2022-12-06 06:51:18 -0600

    week2 update start

--- v2/assets/script.js
+++ v2/assets/script.js
@@ -10,37 +10,38 @@ $(document).ready(function() {
         "&app_id=aa7e15d0&app_key=95bc405156202f2376c3c63ef483565b",
       method: "GET"
     }).then(function(response) {
+      // jw, This is for each Recipe ======================================== !
       var hits = response.hits;
       console.log(response.hits);
       $(".dish-results").empty();
       for (i = 0; i < hits.length; i++) {
         //loops through and makes a new div for each recipe
         var recipeDiv = $("<div>");
-        recipeDiv.addClass("name column is-5 has-text-centered subtitle");
+        recipeDiv.addClass("each_food name is-5 has-text-centered subtitle");
         //adds name of the recipes
         var names = hits[i].recipe.label;
-        recipeDiv.text(names);
+        recipeDiv.text(names); // Recipe name
         //adds div for buttons (style) ****This is so bulma understands how to lay out the buttons
         var resultbuttons = $("<div>");
-        resultbuttons.addClass("columns is-mobile is-centered");
+        resultbuttons.addClass("is-centered");
         recipeDiv.append(resultbuttons);
         //adds links to the divs
         var links = $("<a>");
         var recipeLinks = hits[i].recipe.url;
         console.log(recipeLinks);
         links.addClass(
-          "column is-5-desktop is-5-tablet is-5-mobile search-item"
+          "search-item"
         );
         links.attr("href", recipeLinks);
         links.attr("target", "_blank");
-        links.text("Recepie Source");
+        links.text("Recepie Source"); // "Recipe Source"
         resultbuttons.append(links);
         //adds buttons for adding ingredients
         var ingredientBttn = $("<button>");
-        ingredientBttn.text("Save Ingredients");
+        ingredientBttn.text("Save Ingredients"); // "Save Ingredients"
         ingredientBttn.attr(
           "class",
-          "ingredients-button column is-5-desktop is-5-tablet is-5-mobile search-item"
+          "ingredients-button search-item"
         );
         ingredientBttn.attr("value", hits[i].recipe.ingredientLines);
         ingredientBttn.attr("data-link", recipeLinks);
@@ -77,19 +78,19 @@ $(document).ready(function() {
     var iterator = ingredientsShop.values();
     for (var value of iterator) {
       // creates div for item and button
-      var newDiv = $("<div>").attr("class", "columns is-mobile shopitem");
+      var newDiv = $("<div>").attr("class", "shopitem");
       // creates li //
       var newListItem = $("<li>")
         .attr(
           "class",
-          "listItems column is-four-fifths-desktop is-two-thirds-tablet is-two-thirds-mobile"
+          "listItems"
         )
         .text(value);
       // adds a close button to the list items. //
       var addSpan = $("<button>")
         .attr(
           "class",
-          "column is-2-desktop is-2-tablet is-2-mobile listButton close"
+          "listButton close"
         )
         .text("X");
       // adds item and close span to the new div //
@@ -118,19 +119,19 @@ $(document).ready(function() {
       $("#addItem").val("");
     } else {
       // creates div for item and button
-      var newDiv = $("<div>").attr("class", "columns is-mobile shopitem");
+      var newDiv = $("<div>").attr("class", "shopitem");
       // creates li //
       var newListItem = $("<li>")
         .attr(
           "class",
-          "listItems column is-four-fifths-desktop is-two-thirds-tablet is-two-thirds-mobile"
+          "listItems"
         )
         .text(newItem);
       // adds a close button to the list items. //
       var addSpan = $("<button>")
         .attr(
           "class",
-          "column is-2-desktop is-2-tablet is-2-mobile listButton close"
+          "listButton close"
         )
         .text("X");
       // adds item and close span to the new div //
@@ -171,17 +172,17 @@ $(document).ready(function() {
       values.push( localStorage.getItem(keys[i]));
   }
   for (j = 0; j < values.length; j++) {
-    var newDiv = $("<div>").attr("class", "columns is-mobile shopitem");
+    var newDiv = $("<div>").attr("class", "shopitem");
     var newListItem = $("<li>")
       .attr(
         "class",
-        "listItems column is-four-fifths-desktop is-two-thirds-tablet is-two-thirds-mobile"
+        "listItems"
       )
       .text(values[j]);
     var addSpan = $("<button>")
       .attr(
         "class",
-        "column is-2-desktop is-2-tablet is-2-mobile listButton close"
+        "listButton close"
       )
       .text("X");
     newDiv.append(newListItem, addSpan);
